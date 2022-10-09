64765c0797afeb18dc0b8c194d96a38baa61150a
Muhmad K. Fauzi <muhmad@live.no>
2022-10-09 22:20:52 +0200

    fixed activation function for phoc

--- modules/models.py
+++ modules/models.py
@@ -21,12 +21,10 @@ class PHOSCnet(nn.Module):
 
             # Stride of the convolving kernel
             # padding is used to control the amount of padding applied to the input
-            #nn.Conv2d(3, 64, (3,3), stride=1, padding='same'),
             nn.Conv2d(3, 64, kernel_size =(3, 3), stride = 1, padding='same'),
             nn.ReLU(),
             #Convolution + relu
             nn.Conv2d(64, 64, kernel_size =(3, 3), stride=1, padding='same'),
-            #nn.Conv2d(64, 64, (3,3), stride=1, padding='same'),
             nn.ReLU(),
             # Pooling - Max Pooling
             # Max pooling is a pooling operation that selects the maximum\
@@ -37,50 +35,39 @@ class PHOSCnet(nn.Module):
 
             #Convolution + relu
             nn.Conv2d(64, 128, kernel_size =(3, 3), stride=1, padding='same'),
-            #nn.Conv2d(64, 128, (3,3), stride=1, padding='same'),
             nn.ReLU(),
             #Convolution + relu
             nn.Conv2d(128, 128, kernel_size =(3, 3), stride=1, padding='same'),
-            #nn.Conv2d(128, 128, (3,3), stride=1, padding='same'),
             nn.ReLU(),
             # Pooling
             nn.MaxPool2d(2, 2),
 
             #Convolution + relu
             nn.Conv2d(128, 256, kernel_size =(3, 3), stride=1, padding='same'),
-            #nn.Conv2d(128, 256, (3,3), stride=1, padding='same'),
             nn.ReLU(),
             #Convolution + relu
             nn.Conv2d(256, 256, kernel_size =(3, 3), stride=1, padding='same'),
-            #nn.Conv2d(256, 256, (3,3), stride=1, padding='same'),
             nn.ReLU(),
             #Convolution + relu
             nn.Conv2d(256, 256, kernel_size =(3, 3), stride=1, padding='same'),
-            #nn.Conv2d(256, 256, (3,3), stride=1, padding='same'),
             nn.ReLU(),
             #Convolution + relu
             nn.Conv2d(256, 256, kernel_size =(3, 3), stride=1, padding='same'),
-            #nn.Conv2d(256, 256, (3,3), stride=1, padding='same'),
             nn.ReLU(),
             #Convolution + relu
             nn.Conv2d(256, 256, kernel_size =(3, 3), stride=1, padding='same'),
-            #nn.Conv2d(256, 256, (3,3), stride=1, padding='same'),
             nn.ReLU(),
             #Convolution + relu
             nn.Conv2d(256, 256, kernel_size =(3, 3), stride=1, padding='same'),
-            #nn.Conv2d(256, 256, (3,3), stride=1, padding='same'),
             nn.ReLU(),
             #Convolution + relu
             nn.Conv2d(256, 512, kernel_size =(3, 3), stride=1, padding='same'),
-            #nn.Conv2d(256, 512, (3,3), stride=1, padding='same'),
             nn.ReLU(),
             #Convolution + relu
             nn.Conv2d(512, 512, kernel_size =(3, 3), stride=1, padding='same'),
-            #nn.Conv2d(512, 512, (3,3), stride=1, padding='same'),
             nn.ReLU(),
             #Convolution + relu
             nn.Conv2d(512, 512, kernel_size =(3, 3), stride=1, padding='same'),
-            #nn.Conv2d(512, 512, (3,3), stride=1, padding='same'),
             nn.ReLU(),
 
         )
@@ -108,7 +95,7 @@ class PHOSCnet(nn.Module):
             nn.Linear(4096, 4096),
             nn.ReLU(),
             nn.Dropout(0.5),
-            nn.Linear(4096, 604),
+            nn.Sigmoid(4096, 604),
             nn.ReLU()
         )
 
